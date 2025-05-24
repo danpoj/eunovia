39fb3fe82f611c2f2dabb81972189fc7af312b82
danpoj <jws970306@khu.ac.kr>
2025-05-24 13:36:00 +0900

    prettier, initial setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  singleAttributePerLine: true,
+}
